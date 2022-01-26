36b96a21a3cebfa7afac3326267610ca16fe1c9a
csgnwinter <93068145+csgnwinter@users.noreply.github.com>
2022-01-26 11:18:23 +0800

    Add files via upload

---
+++ q_2a.java
@@ -0,0 +1,6 @@
+public class q_2a {
+    public static void main (String[] args) {
+        String name = "Ayan";
+        System.out.println("Hello, my name is "+name+"!");
+    }
+}
---
+++ q_2c.java
@@ -0,0 +1,11 @@
+public class q_2c {
+    public static void main (String[] args){
+        for(int x = 102; x>=66; x=x-1){
+            if(x%2==1){
+                System.out.print("value of x: " + x);
+                System.out.print("\n");
+            }
+
+        }
+    }
+}
